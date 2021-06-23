747626ca223e48dc1489016ad8a7a4bce153b536
Quinn Webber <webber.quinn@gmail.com>
2021-06-23 10:27:31 -0230

    Update 11-LAR.R

--- R/11-LAR.R
+++ R/11-LAR.R
@@ -10,7 +10,7 @@ libs <- c('data.table', 'ggplot2', 'asnipe', 'spatsoc', 'plotrix', 'gridExtra')
 lapply(libs, require, character.only = TRUE)
 
 ### Input raw data ----
-fogo <- readRDS("output/7-locs-mod.RDS")
+fogo <- readRDS("output/location-data/7-locs-mod.RDS")
 fogo$Year <- as.factor(fogo$Year)
 
 ## remove 
@@ -18,6 +18,97 @@ fogo <- fogo[!is.na(season)]
 
 fogo$IDmemb <- as.factor(paste(fogo$ANIMAL_ID, fogo$membership, sep = "_"))
 
+
+######################## 
+######## 2017 ######### 
+########################
+
+fo17 <- get_gbi(fogo[Year == "2017"], group = 'group', id = 'ANIMAL_ID')
+times17 <- data.table(time = fogo[Year == "2017"]$JDate,
+                      group = fogo[Year == "2017"]$group)
+days <- data.table(group = as.numeric(rownames(fo17)))
+qq <- merge(times17, days, by = "group")
+times17 <- unique(qq)
+times17 <- times17$time
+
+lra17 <- data.table(LRA(fo17, times = times17, timejump = 1, output_style = 2))
+LRA17avg <- lra17[, meanLRA := mean(RATE), by = .(TIME)]
+LRA17avg <- lra17[, seLRA := std.error(RATE), by = .(TIME)]
+avg17 <- LRA17avg[, unique(meanLRA), by = c("TIME")]
+se17 <- LRA17avg[, unique(seLRA), by = c("TIME")][,c("TIME") := NULL]
+LRA17 <- cbind(avg17, se17)
+colnames(LRA17) <- c("TIME", "meanLRA", "seLRA")
+LRA17$Year <- "2017"
+
+######################## 
+######## 2018 ######### 
+########################
+
+fo18 <- get_gbi(fogo[Year == "2018"], group = 'group', id = 'ANIMAL_ID')
+times18 <- data.table(time = fogo[Year == "2018"]$JDate,
+                      group = fogo[Year == "2018"]$group)
+days <- data.table(group = as.numeric(rownames(fo18)))
+qq <- merge(times18, days, by = "group")
+times18 <- unique(qq)
+times18 <- times18$time
+
+lra18 <- data.table(LRA(fo18, times = times18, timejump = 1, output_style = 2))
+LRA18avg <- lra18[, meanLRA := mean(RATE), by = .(TIME)]
+LRA18avg <- lra18[, seLRA := std.error(RATE), by = .(TIME)]
+avg18 <- LRA18avg[, unique(meanLRA), by = c("TIME")]
+se18 <- LRA18avg[, unique(seLRA), by = c("TIME")][,c("TIME") := NULL]
+LRA18 <- cbind(avg18, se18)
+colnames(LRA18) <- c("TIME", "meanLRA", "seLRA")
+LRA18$Year <- "2018"
+
+######################## 
+######## 2019 ######### 
+########################
+
+fo19 <- get_gbi(fogo[Year == "2019"], group = 'group', id = 'ANIMAL_ID')
+times19 <- data.table(time = fogo[Year == "2019"]$JDate,
+                      group = fogo[Year == "2019"]$group)
+days <- data.table(group = as.numeric(rownames(fo19)))
+qq <- merge(times19, days, by = "group")
+times19 <- unique(qq)
+times19 <- times19$time
+
+lra19 <- data.table(LRA(fo19, times = times19, timejump = 1, output_style = 2))
+LRA19avg <- lra19[, meanLRA := mean(RATE), by = .(TIME)]
+LRA19avg <- lra19[, seLRA := std.error(RATE), by = .(TIME)]
+avg19 <- LRA19avg[, unique(meanLRA), by = c("TIME")]
+se19 <- LRA19avg[, unique(seLRA), by = c("TIME")][,c("TIME") := NULL]
+LRA19 <- cbind(avg19, se19)
+colnames(LRA19) <- c("TIME", "meanLRA", "seLRA")
+LRA19$Year <- "2019"
+
+LAR <- rbind(LRA17, LRA18, LRA19)
+
+png("graphics/Fig2.png", width = 3000, height = 3000,
+    units = "px", res = 600)
+ggplot(LAR, aes(TIME, meanLRA)) +
+  geom_line(aes(color = as.factor(Year)), alpha = 0.5, size = 1) + 
+  ylab("Lagged association rate") +
+  xlab("Days") +
+  ylim(0, 0.7) +
+  geom_errorbar(aes(ymin = meanLRA - seLRA, 
+                    ymax = meanLRA + seLRA, 
+                color = as.factor(Year)),
+                width=0.04, alpha = 0.5) + 
+  scale_color_manual(values = c("#7570b3", "#d95f02", "#1b9e77")) +
+  labs(color = "Year") +
+  theme(legend.position = c(0.15,0.85),
+        legend.title = element_text(size = 12),
+        legend.key = element_blank(),
+        legend.text = element_text(size = 10),
+        axis.title = element_text(size = 14, color = 'black'),
+        axis.text = element_text(size = 12, color = 'black'),
+        plot.title=element_text(size = 14, hjust=0),
+        axis.line = element_line(colour = "black"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        panel.border = element_rect(colour = "black", fill=NA, size = 1)) 
+dev.off()
 ######################## 
 ######## 2017 ######### 
 ########################
@@ -194,7 +285,7 @@ colnames(LRA19) <- c("TIME", "memb", "avg", "se")
 
 color = c("orange", "dodgerblue")
 
-png("graphics/Fig2.png", width = 6000, height = 3000,
+png("graphics/FigS4.png", width = 6000, height = 3000,
     units = "px", res = 600)
 aa = ggplot(LRA17, aes(TIME, avg, fill = factor(memb))) +
   geom_line(alpha = 0.5, aes(color = factor(memb)), size = 1) + 
