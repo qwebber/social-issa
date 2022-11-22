f8d9bcd4b46ae9cabcd01debda8a854cd8389fcd
Quinn Webber <webber.quinn@gmail.com>
2022-11-22 14:00:23 -0500

    Update 10-issa.R

--- R/10-issa.R
+++ R/10-issa.R
@@ -13,72 +13,49 @@ DT[, .N, by = c("IDYr")]
 DT$Use[DT$case_ == "TRUE"] <- 1
 DT$Use[DT$case_ == "FALSE"] <- 0
 
+## check overlap between used/available points
+ggplot(DT[step_id_ == 228 & ANIMAL_ID == "FO2018002"]) +
+  geom_point(aes(x2_, y2_, color = as.factor(Use)), pch=21,
+                            size=4)
+
 ## number of groups
 length(unique(DT[iter == 1]$groupEnd))
 
 ## create unique step id by animal
 DT[,'caribou_step_id_'] <- paste(DT$IDYr, DT$step_id_, sep = '_')
 
-### ISSF MODEL WITH NN
-NN_20 <- glmmTMB(case_ ~ 
-                          ## step length
-                          I(log(sl_+1)) + 
-                          I(log(sl_+1)):(propOpenMove + propForest + propLichen) +
-                          
-                          ## habitat variables
-                          propOpenMove + propForest + propLichen +
-                          
-                          ## social variables in interactions with movement and habitat 
-                          I(log(EndDist + 1)) + 
-                          I(log(StartDist + 1)):I(log(sl_+1)) +
-                          I(log(EndDist + 1)):(propOpenMove + propForest + propLichen) + 
-                          
-                          ## random effects  
-                          (1|caribou_step_id_) + 
-                          (0 + I(log(sl_+1)) | IDYr) +
-                          (0 + propOpenMove:I(log(EndDist+1)) | IDYr) +
-                          (0 + propForest:I(log(EndDist+1)) | IDYr) +
-                          (0 + propLichen:I(log(EndDist+1)) | IDYr) +
-                          (0 + I(log(EndDist+1)) | IDYr) +
-                          (0 + I(log(StartDist + 1)):I(log(sl_+1)) | IDYr),
-                        
-                        family=poisson(), 
-                        data = DT,  
-                        map = list(theta=factor(c(NA,1:6))), 
-                        start = list(theta=c(log(1000), seq(0,0, length.out = 6))))
-summary(NN_20)
-check_collinearity(NN_20)
-
-saveRDS(NN_20, "output/issa models/NN_issa_20.RDS")
-
-
 ### ISSF MODEL WITH NN and SRI 
 SRI_20 <- glmmTMB(case_ ~ 
                          ## step length
                          I(log(sl_+1)) + 
-                         I(log(sl_+1)):(propOpenMove + propForest + propLichen) +
+                         I(log(sl_+1)):(#propOpenMove + 
+                           propForest + propLichen) +
                          
                          ## habitat variables
-                         propOpenMove + propForest + propLichen +
+                         #propOpenMove + 
+                         propForest + 
+                         propLichen +
                           
                          ## social variables in interactions with movement and habitat 
                          I(log(EndDist + 1)) + 
                          I(log(StartDist + 1)):I(log(sl_+1)) +
-                         I(log(EndDist + 1)):(propOpenMove + propForest + propLichen) + 
+                         I(log(EndDist + 1)):(#propOpenMove + 
+                           propForest + propLichen) + 
                     
                          I(log(sri+0.125)) +
                          I(log(sri+0.125)):I(log(sl_+1)) +
-                         I(log(sri+0.125)):(propOpenMove + propForest + propLichen) + 
+                         I(log(sri+0.125)):(#propOpenMove + 
+                           propForest + propLichen) + 
                           
                          ## random effects  
                           (1|caribou_step_id_) + 
                           (0 + I(log(sl_+1)) | IDYr) +
-                          (0 + propOpenMove:I(log(sri+0.125))| IDYr) +
+                          #(0 + propOpenMove:I(log(sri+0.125))| IDYr) +
                           (0 + propForest:I(log(sri+0.125))| IDYr) +
                           (0 + propLichen:I(log(sri+0.125))| IDYr) +
                           (0 + I(log(sri+0.125)) | IDYr) +
                           (0 + I(log(sl_+1)):I(log(sri+0.125))| IDYr) +
-                          (0 + propOpenMove:I(log(EndDist+1)) | IDYr) +
+                          #(0 + propOpenMove:I(log(EndDist+1)) | IDYr) +
                           (0 + propForest:I(log(EndDist+1)) | IDYr) +
                           (0 + propLichen:I(log(EndDist+1)) | IDYr) +
                           (0 + I(log(EndDist+1)) | IDYr) +
@@ -86,20 +63,11 @@ SRI_20 <- glmmTMB(case_ ~
                         
                        family=poisson(), 
                        data = DT,  
-                       map = list(theta=factor(c(NA,1:11))), 
-                       start = list(theta=c(log(1000), seq(0,0, length.out = 11))))
+                       map = list(theta=factor(c(NA,1:9))), 
+                       start = list(theta=c(log(1000), seq(0,0, length.out = 9))))
 
-summary(SRI_20)
+ summary(SRI_20)
 check_collinearity(SRI_20)
 
-saveRDS(SRI_20, "output/issa models/SRI_issa_20.RDS")
+saveRDS(SRI_20, "output/issa models/SRI_issa_20_2.RDS")
 
-##assign model names
-Modnames <- c("NN", "SRI")
-
-##compute model selection table
-aicctable.out <- aictab(cand.set = list(NN_20, SRI_20), 
-                        modnames = Modnames)
-
-##compute evidence ratio
-evidence(aic.table = aicctable.out)
